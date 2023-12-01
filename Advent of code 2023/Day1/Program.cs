29a5f612aab4cfb7e18d9e51a9af4e5418124075
Jasper Huizing <jasper.huizing@wearetriple.com>
2023-12-01 07:55:26 +0100

    day 1 part 1

---
+++ Advent of code 2023/Day1/Program.cs
@@ -0,0 +1,40 @@
+﻿await Part1();
+
+static async Task Part1()
+{
+    var input = await File.ReadAllLinesAsync("input.txt");
+
+    var sum = 0;
+
+    foreach (var line in input)
+    {
+        var firstNumber = "";
+        var LastNumber = "";
+
+        foreach (var character in line)
+        {
+            if (int.TryParse(character.ToString(), out var number))
+            {
+                if (string.IsNullOrEmpty(firstNumber))
+                {
+                    firstNumber = character.ToString();
+                }
+
+                if (string.IsNullOrEmpty(LastNumber))
+                {
+                    LastNumber = character.ToString();
+                }
+
+                if (LastNumber != character.ToString())
+                {
+                    LastNumber = character.ToString();
+                }
+            }
+        }
+
+        sum += int.Parse($"{firstNumber}{LastNumber}");
+
+    }
+
+    Console.WriteLine($"Sum of calibration values is: {sum}");
+}
